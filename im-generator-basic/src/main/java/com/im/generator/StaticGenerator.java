22db73fc6dd87913cfddd09de21c02aee1b97d75
stockimen <stockimen@qq.com>
2025-04-05 12:13:09 +0800

    组合调用动态、静态生成

--- im-generator-basic/src/main/java/com/im/generator/StaticGenerator.java
+++ im-generator-basic/src/main/java/com/im/generator/StaticGenerator.java
@@ -38,6 +38,7 @@ public class StaticGenerator {
 
     /**
      * 递归拷贝文件（递归实现，会将输入目录完整拷贝到输出目录下）
+     *
      * @param inputPath
      * @param outputPath
      */
@@ -56,8 +57,9 @@ public class StaticGenerator {
      * 文件 A => 目录 B，则文件 A 放在目录 B 下
      * 文件 A => 文件 B，则文件 A 覆盖文件 B
      * 目录 A => 目录 B，则目录 A 放在目录 B 下
-     *
+     * <p>
      * 核心思路：先创建目录，然后遍历目录内的文件，依次复制
+     *
      * @param inputFile
      * @param outputFile
      * @throws IOException
@@ -65,7 +67,7 @@ public class StaticGenerator {
     private static void copyFileByRecursive(File inputFile, File outputFile) throws IOException {
         // 区分是文件还是目录
         if (inputFile.isDirectory()) {
-            System.out.println(inputFile.getName());
+            System.out.println("路径：" + inputFile.getName());
             File destOutputFile = new File(outputFile, inputFile.getName());
             // 如果是目录，首先创建目标目录
             if (!destOutputFile.exists()) {
