365643a55f5b5050a5d545b824ca2b6cad997d67
stockimen <stockimen@qq.com>
2025-04-08 18:36:42 +0800

    简单命令行试验

---
+++ im-generator-basic/src/main/java/com/im/cli/example/Login.java
@@ -0,0 +1,31 @@
+package com.im.cli.example;
+
+import java.util.concurrent.Callable;
+import picocli.CommandLine.Command;
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+public class Login implements Callable<Integer> {
+    @Option(names = {"-u", "--user"}, description = "User name")
+    String user;
+
+    // 设置了 arity 参数，可选交互式
+    @Option(names = {"-p", "--password"}, arity = "0..1", description = "Passphrase", interactive = true)
+    String password;
+
+    // 设置了 arity 参数，可选交互式
+    @Option(names = {"-cp", "--checkPassword"}, arity = "0..1", description = "Check Password", interactive = true)
+    String checkPassword;
+
+    public Integer call() throws Exception {
+        System.out.println("User: "+ user);
+        System.out.println("password = " + password);
+        System.out.println("checkPassword = " + checkPassword);
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        new CommandLine(new Login()).execute("-u", "aa");
+    }
+}
