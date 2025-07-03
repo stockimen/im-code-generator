82f74c3cb570988510b02f80893e0bef0cf44e8f
stockimen <stockimen@qq.com>
2025-07-03 11:20:20 +0800

    制作工具优化-可移植性优化、功能优化、健壮性优化、可扩展性优化

--- im-generator-maker/src/main/java/com/im/maker/Main.java
+++ im-generator-maker/src/main/java/com/im/maker/Main.java
@@ -2,9 +2,14 @@ package com.im.maker;
 
 //import com.im.maker.cli.CommandExecutor;
 
+import com.im.maker.generator.main.MainGenerator;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-//        CommandExecutor commandExecutor = new CommandExecutor();
-//        commandExecutor.doExecute(args);
+    public static void main(String[] args) throws TemplateException, IOException, InterruptedException {
+        MainGenerator mainGenerator = new MainGenerator();
+        mainGenerator.doGenerate();
     }
 }
