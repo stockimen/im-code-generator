1396b5e8eabc0eb0dc498b5cb1158d8cfe8f67a0
stockimen <stockimen@qq.com>
2025-07-25 00:15:29 +0800

    模板制作工具-修复bug：错误处理了新生成的模板文件

--- im-generator-maker/src/test/java/com/im/maker/template/TemplateMakerTest.java
+++ im-generator-maker/src/test/java/com/im/maker/template/TemplateMakerTest.java
@@ -46,4 +46,36 @@ public class TemplateMakerTest {
         System.out.println(id);
     }
 
+    /**
+     * 同文件目录多次生成时，会扫描新的 FTL 文件
+     */
+    @Test
+    public void testMakeTemplateBug2() {
+        Meta meta = new Meta();
+        meta.setName("acm-template-generator");
+        meta.setDescription("ACM 示例模板生成器");
+
+        String projectPath = System.getProperty("user.dir");
+        String originProjectPath = new File(projectPath).getParent() + File.separator + "yuzi-generator-demo-projects/springboot-init";
+
+        // 文件参数配置，扫描目录
+        String inputFilePath1 = "src/main/java/com/yupi/springbootinit/common";
+        TemplateMakerFileConfig templateMakerFileConfig = new TemplateMakerFileConfig();
+        TemplateMakerFileConfig.FileInfoConfig fileInfoConfig1 = new TemplateMakerFileConfig.FileInfoConfig();
+        fileInfoConfig1.setPath(inputFilePath1);
+        templateMakerFileConfig.setFiles(Arrays.asList(fileInfoConfig1));
+
+        // 模型参数配置
+        TemplateMakerModelConfig templateMakerModelConfig = new TemplateMakerModelConfig();
+        TemplateMakerModelConfig.ModelInfoConfig modelInfoConfig1 = new TemplateMakerModelConfig.ModelInfoConfig();
+        modelInfoConfig1.setFieldName("className");
+        modelInfoConfig1.setType("String");
+        modelInfoConfig1.setReplaceText("BaseResponse");
+        List<TemplateMakerModelConfig.ModelInfoConfig> modelInfoConfigList = Arrays.asList(modelInfoConfig1);
+        templateMakerModelConfig.setModels(modelInfoConfigList);
+
+        long id = TemplateMaker.makeTemplate(meta, originProjectPath, templateMakerFileConfig, templateMakerModelConfig, 1735281524670181376L);
+        System.out.println(id);
+    }
+
 }
